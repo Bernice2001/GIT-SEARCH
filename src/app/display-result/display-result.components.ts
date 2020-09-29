5aeb49ebf914f30c48c6506dcf086077526968d6
Bernice2001 <wangechibanim@gmail.com>
2020-09-29 13:12:50 +0300

    .json files done

---
+++ src/app/display-result/display-result.components.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../user-request/request.service';
+
+@Component({
+  selector: 'app-display-result',
+  templateUrl: './display-result.component.html',
+  providers:[RequestService],
+  styleUrls: ['./display-result.component.css']
+})
+export class DisplayResultComponent implements OnInit {
+
+  constructor(private requestsearvice:RequestService) { }
+
+  ngOnInit(): void {
+  }
+
+}
